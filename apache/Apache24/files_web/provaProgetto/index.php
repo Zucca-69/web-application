12203b065bd9a3622961582e65c99b531d4ca045
Zucca_69 <nicozuccarino@gmail.com>
2025-04-18 12:15:26 +0200

    ADDED : database di base, files apache & php (test). CHANGED : spostato la cartella PAGINE nella document root di apache (files_web)

---
+++ apache/Apache24/files_web/provaProgetto/index.php
@@ -0,0 +1,71 @@
+<?php
+    require_once __DIR__ . '/db.php';
+
+    // Get file count in table
+    $sql = "SELECT COUNT(*) FROM immagini";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_row();
+    echo 'Total of registers: '. $row[0];
+
+    // Validate the limit of files allowed in the database
+    if($row[0] < 15){
+        // Check if $_FILES count items
+        if (count($_FILES) > 0) {
+            // Check if file was uploaded
+            if (is_uploaded_file($_FILES['userImage']['tmp_name'])) {
+                // Insert image as MySQL BLOB usign file_get_contents() to reads entire file into a string
+                $imgData = file_get_contents($_FILES['userImage']['tmp_name']);
+                $imgName = $_FILES['userImage']['name'];
+                $imgType = $_FILES['userImage']['type'];
+                $imgSize = $_FILES['userImage']['size'];
+                $imgTmpName = $_FILES['userImage']['tmp_name'];
+                $imgError = $_FILES['userImage']['error'];
+                $imgURL;
+                $sql = "INSERT INTO immagini(imageData, imageName, imageType, imageSize, imageTmpName, imageError, imageURL, upload_on) VALUES(?, ?, ?, ?, ?, ?, ?, NOW())";
+                $statement = $conn->prepare($sql);
+                $statement->bind_param('sssssss', $imgData, $imgName, $imgType, $imgSize, $imgTmpName, $imgError, $imgURL);
+                $current_id = $statement->execute() or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_connect_error());
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    } else {
+        echo "<div style='text-align: center; padding: 30px 0 10px 0; font-size: 20px; color: #c0392b'>
+        The allowed file limit has been reached <br/> Uploading more files is not allowed due to my condition code <br/> Thanks for coming!</div>";
+    }
+?>
+
+<HTML>
+    <HEAD>
+        <TITLE>PHP | Upload BLOB files in MySQL</TITLE>
+        <meta charset="UTF-8">
+        <meta name="description" content="Example of upload BLOB files in MySQL using PHP">
+        <meta name="keywords" content="PHP, MySQL, BLOB, Files, Upload">
+        <meta name="author" content="JORGE LUIS AGUIRRE MARTINEZ">
+        <meta name="publish_date" property="og:publish_date" content="2023-03-10T18:00:00-0600">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="css/form.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </HEAD>
+
+    <BODY>
+        <!--- Upload the image file --->
+        <form name="frmImage" enctype="multipart/form-data" action="" method="post">
+            <div class="phppot-container tile-container">
+                <label>Upload image file:</label>
+                <div class="row">
+                    <input name="userImage" type="file" class="full-width" />
+                </div>
+                <div class="row">
+                    <input type="submit" value="Submit" />
+                </div>
+            </div>
+            <div class="image-gallery">
+                <?php require_once __DIR__ . '/listImages.php'; ?>
+            </div>
+        </form>
+    </BODY>
+</HTML>
---
+++ rungamedb.sql
@@ -0,0 +1,148 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versione server:              11.7.2-MariaDB - mariadb.org binary distribution
+-- S.O. server:                  Win64
+-- HeidiSQL Versione:            12.10.0.7000
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dump della struttura del database rungamedb
+CREATE DATABASE IF NOT EXISTS `rungamedb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+USE `rungamedb`;
+
+-- Dump della struttura di tabella rungamedb.appartenenze
+CREATE TABLE IF NOT EXISTS `appartenenze` (
+  `FKcategoryId` int(11) NOT NULL,
+  `FKproductId` int(11) NOT NULL,
+  PRIMARY KEY (`FKcategoryId`,`FKproductId`),
+  KEY `FK_appartenenze_prodotti` (`FKproductId`),
+  CONSTRAINT `FK_appartenenze_categorie` FOREIGN KEY (`FKcategoryId`) REFERENCES `categorie` (`categoryId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_appartenenze_prodotti` FOREIGN KEY (`FKproductId`) REFERENCES `prodotti` (`productId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci COMMENT='interazione tra categorie e prodotti';
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella rungamedb.carrelli
+CREATE TABLE IF NOT EXISTS `carrelli` (
+  `cartId` int(11) NOT NULL AUTO_INCREMENT,
+  `quantità` int(11) NOT NULL,
+  PRIMARY KEY (`cartId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella rungamedb.categorie
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `categoryId` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella rungamedb.immagini
+CREATE TABLE IF NOT EXISTS `immagini` (
+  `imageId` int(11) NOT NULL AUTO_INCREMENT,
+  `imageData` longblob NOT NULL,
+  `imageName` varchar(255) NOT NULL DEFAULT '',
+  `imageType` varchar(255) NOT NULL,
+  `imageSize` varchar(45) NOT NULL,
+  `imageTmpName` varchar(255) DEFAULT NULL,
+  `imageError` varchar(45) DEFAULT NULL,
+  `imageURL` varchar(255) DEFAULT NULL,
+  `uploaded_on` datetime NOT NULL,
+  `status` enum('1','0') NOT NULL DEFAULT '1',
+  `FKcategoryId` int(11) DEFAULT NULL,
+  `FKproductId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`imageId`),
+  KEY `FK_immagini_carrelli` (`FKcategoryId`),
+  KEY `FK_immagini_prodotti` (`FKproductId`),
+  CONSTRAINT `FK_immagini_carrelli` FOREIGN KEY (`FKcategoryId`) REFERENCES `categorie` (`categoryId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_immagini_prodotti` FOREIGN KEY (`FKproductId`) REFERENCES `prodotti` (`productId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella rungamedb.interazioni
+CREATE TABLE IF NOT EXISTS `interazioni` (
+  `FKuserId` int(11) NOT NULL,
+  `FKproductId` int(11) NOT NULL,
+  `FKcartId` int(11) NOT NULL,
+  `tipologia` enum('visualizzato','messo nel carrello','acquistato') NOT NULL,
+  `timestamp` timestamp NOT NULL,
+  PRIMARY KEY (`FKuserId`,`FKproductId`,`FKcartId`),
+  KEY `FK_interazioni_prodotti` (`FKproductId`),
+  KEY `FK_interazioni_carrelli` (`FKcartId`),
+  CONSTRAINT `FK_interazioni_carrelli` FOREIGN KEY (`FKcartId`) REFERENCES `carrelli` (`cartId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_interazioni_prodotti` FOREIGN KEY (`FKproductId`) REFERENCES `prodotti` (`productId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_interazioni_utenti` FOREIGN KEY (`FKuserId`) REFERENCES `utenti` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci COMMENT='tabella di interazione tra utenti prodotti e carrello';
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella rungamedb.metodipagamento
+CREATE TABLE IF NOT EXISTS `metodipagamento` (
+  `numeroCartaHash` varchar(80) NOT NULL DEFAULT '',
+  `nomeCarta` varchar(35) NOT NULL,
+  `tipologia` enum('visa','paypal','mastercard','revolut') NOT NULL,
+  `scadenza` datetime NOT NULL,
+  `cvvHash` varchar(80) NOT NULL DEFAULT '',
+  `soprannomeCarta` varchar(35) DEFAULT NULL,
+  `FKuserId` int(11) NOT NULL,
+  PRIMARY KEY (`numeroCartaHash`) USING BTREE,
+  KEY `userId_pay` (`FKuserId`) USING BTREE,
+  CONSTRAINT `FK_metodipagamento_utenti` FOREIGN KEY (`FKuserId`) REFERENCES `utenti` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella rungamedb.prodotti
+CREATE TABLE IF NOT EXISTS `prodotti` (
+  `productId` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL DEFAULT '',
+  `descrizione` varchar(50) NOT NULL DEFAULT '',
+  `prezzo` float NOT NULL DEFAULT 0,
+  `saga` varchar(50) DEFAULT NULL,
+  `piattaforma` enum('PS4','PS5','PC','XBOX360','XBOXONE') DEFAULT NULL,
+  `quantitaDisponibile` int(11) NOT NULL,
+  `dataUscita` date NOT NULL,
+  `dataDisponibile` date DEFAULT NULL,
+  PRIMARY KEY (`productId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella rungamedb.utenti
+CREATE TABLE IF NOT EXISTS `utenti` (
+  `userId` int(4) NOT NULL AUTO_INCREMENT,
+  `nome` char(50) NOT NULL,
+  `cognome` char(35) NOT NULL,
+  `dataNascita` date NOT NULL DEFAULT '0000-00-00',
+  `dataIscrizione` date NOT NULL DEFAULT '0000-00-00',
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `passwordHash` varchar(80) NOT NULL DEFAULT '',
+  `codiceVerifica` varchar(10) NOT NULL,
+  `verificato` bit(1) NOT NULL DEFAULT b'0',
+  `FKimmagineProfilo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userId`),
+  KEY `immagineProfilo` (`FKimmagineProfilo`) USING BTREE,
+  CONSTRAINT `FK_utenti_immagini` FOREIGN KEY (`FKimmagineProfilo`) REFERENCES `immagini` (`imageId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- L’esportazione dei dati non era selezionata.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
