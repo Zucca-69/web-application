5252d4f8eb0766439e74d61a2cd0db9fafa5b073
Zucs <zuccarinonico@gmail.com>
2025-05-12 10:51:04 +0200

    CHANGED: se sei loggato con l'admin (userId = 0) allora vedi le pagine di aggiunte prodotti

--- files_web/php_files/header_check.php
+++ files_web/php_files/header_check.php
@@ -1,10 +1,13 @@
-
 <link rel="stylesheet" href="../css/header.css">
 <link rel="stylesheet" href="../css/darkmode.css">
 <script src="../js/dropdown_botton.js" defer></script>
 <script src="../js/theme_toggle.js"></script>
 
 <header>
+    <script>
+        let userId = <?= json_encode($_SESSION['userId'] ?? null) ?>;
+        console.log("User ID:", userId); // Debug
+    </script>
     <div class="navbar-container">
         <!-- Logo a sinistra -->
         <div class="logo">
@@ -13,7 +16,6 @@
             </a>
         </div>
 
-        <!-- Navigazione principale -->
         <ul class="nav-links">
             <li><a href="index.php">Home</a></li>
             <li><a href="news.php">News</a></li>
@@ -22,6 +24,11 @@
             <li><a href="catalogo.php?categoria=">Categorie</a></li>
             <li><a href="contact.php">Contact</a></li>
             <li><a href="servizio-clienti.php">Servizio clienti</a></li>
+
+            <?php if (isset($_SESSION['userId']) && $_SESSION['userId'] == 0): ?>
+                <li><a href="add_prodotto.php">Aggiunta prodotto</a></li>
+                <li><a href="../php_files/add_img.php">Aggiunta immagini</a></li>
+            <?php endif; ?>
         </ul>
 
         <!-- Utente (login o profilo/carrello) -->
@@ -29,7 +36,7 @@
             <?php if (isset($_SESSION['userId'])): ?>
                 <div class="dropdown">
                     <button class="dropbtn">
-                    <img src="../php_files/get_profile_img.php" alt="Immagine profilo" style="width:50px; height:50px;">
+                        <img src="../php_files/get_profile_img.php" alt="Immagine profilo" style="width:50px; height:50px;">
                     </button>
                     <div class="dropdown-content">
                         <a href="profilo.php">Profilo</a>
@@ -37,7 +44,6 @@
                         <a href="../php_files/logout.php">Logout</a>
                     </div>
                 </div>
-
                 <div class="cart-icon">
                     <a href="carrello.html">
                         <img src="../MEDIA/immagini/carrello.png" alt="Carrello">
